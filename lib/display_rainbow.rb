ac0c4bc1a9227e4cf06ea965cf4661c1a2d1454c
Senada Kadric <skadric@fordham.edu>
2020-02-23 16:37:43 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -9,6 +9,6 @@ def display_rainbow(colors)
 
 =end
 
-puts colors[0]
+puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}
 
 end
